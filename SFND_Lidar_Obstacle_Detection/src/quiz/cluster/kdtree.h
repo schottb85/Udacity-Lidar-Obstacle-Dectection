85d5b12bcf804b1e8a127c259ad38ff3165b5133
Benedikt Schott <benedikt.schott@tesis.de>
2019-07-29 07:42:53 +0200

    LIDAR - Clustering - 6.Insert into kd-tree

--- SFND_Lidar_Obstacle_Detection/src/quiz/cluster/kdtree.h
+++ SFND_Lidar_Obstacle_Detection/src/quiz/cluster/kdtree.h
@@ -25,11 +25,32 @@ struct KdTree
 	: root(NULL)
 	{}
 
+	void insertHelperRecursive(Node** node, uint depth, const std::vector<float>& point, int id){
+		//TODO: fill this function
+
+		if(*node == NULL){
+			*node = new Node(point, id);
+			return;
+		}
+
+		// which dimension is split by this node?
+		int dimension = depth % point.size(); // x,y,z, x, y,z, ... (for octree), x, y, x, y, ... (for quadtree)
+
+		// insert point into next leaf - left/right if respective dimension (based on depth) </>= node's dimension
+		Node ** next_leaf = (point[dimension] < (*node)->point[dimension]) ? &(*node)->left : &(*node)->right;
+
+		insertHelperRecursive(next_leaf, depth+1, point, id);
+
+		return;
+	}
+
 	void insert(std::vector<float> point, int id)
 	{
 		// TODO: Fill in this function to insert a new point into the tree
 		// the function should create a new node and place correctly with in the root 
 
+		// TODO: call insertHelperRecursive
+		insertHelperRecursive(&root, 0, point, id);
 	}
 
 	// return a list of point ids in the tree that are within distance of target
